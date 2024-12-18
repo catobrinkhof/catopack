fcdbbe1f7ba86207b5a66e26358db7c5ffeac5f6
catobrinkhof <catobrinkhof@live.nl>
2024-12-18 15:01:03 +0100

    Create complex.R

---
+++ R/complex.R
@@ -0,0 +1,13 @@
+complex <- function(x){
+if(x>0){
+  print("Positive number")
+  }
+else if(x==0)
+  {
+  print("This is just zero! Not positive or negative")
+  }
+else if(x<0)
+  {
+  print("Negative number")
+  }
+}
